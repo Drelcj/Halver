f31330564630d6b360e562b58702b00f6f8aa065
Emmanuel Chijioke <manuelckay@gmail.com>
2022-09-10 21:31:27 +0100

    fan section

--- src/components/fans/fans.jsx
+++ src/components/fans/fans.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import './fans.css'
+import DREL from '../../assets/drel.jpg'
 
 export const Fans = () => {
   return (
-    <section id='fans'>{/*fans*/}</section>
+    <section id='fans'>{/*fans = testimonials section*/}
+      <h5> Super Happy community</h5>
+      <h2>Fans</h2>
+      <div className="container fans__container">
+        <article className="fan">
+          <div className="client__avatar">
+            <img src={DREL} alt="Avatar One" />
+            <h5 className='client__name'>Techman Drel</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eum voluptatem, soluta assumenda, ullam nisi cum porro expedita nulla veritatis dignissimos veniam culpa alias tempore.
+            </small>
+          </div>
+        </article>
+        <article className="fan">
+          <div className="client__avatar">
+            <img src={DREL} alt="Avatar One" />
+            <h5 className='client__name'>Techman Drel</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eum voluptatem, soluta assumenda, ullam nisi cum porro expedita nulla veritatis dignissimos veniam culpa alias tempore.
+            </small>
+          </div>
+        </article>
+        <article className="fan">
+          <div className="client__avatar">
+            <img src={DREL} alt="Avatar One" />
+            <h5 className='client__name'>Techman Drel</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eum voluptatem, soluta assumenda, ullam nisi cum porro expedita nulla veritatis dignissimos veniam culpa alias tempore.
+            </small>
+          </div>
+        </article>
+        <article className="fan">
+          <div className="client__avatar">
+            <img src={DREL} alt="Avatar One" />
+            <h5 className='client__name'>Techman Drel</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eum voluptatem, soluta assumenda, ullam nisi cum porro expedita nulla veritatis dignissimos veniam culpa alias tempore.
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
